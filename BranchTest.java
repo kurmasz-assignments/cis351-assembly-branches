6be5448c00615307e5e272de8480e2124698248d
Zachary Kurmas <kurmasz@gvsu.edu>
2020-10-19 14:50:48 -0400

    Initial Commit

---
+++ BranchTest.java
@@ -0,0 +1,96 @@
+import org.junit.*;
+
+import static edu.gvsu.mipsunit.munit.MUnit.Register.*;
+import static edu.gvsu.mipsunit.munit.MUnit.*;
+
+public class BranchTest {
+
+
+    /******************************************************************
+     *
+     * Test makes10
+     *
+     *****************************************************************/
+
+    @Test
+    public void makes10_aIs10() {
+	run("makes10", 10, 3);
+	Assert.assertEquals(1, get(v0));
+    }
+
+    // Write more tests!
+
+
+    /******************************************************************
+     *
+     * Test intMax
+     *
+     *****************************************************************/
+
+    @Test
+    public void intMax_ascending() {
+	run("intMax", 5, 6, 7);
+	Assert.assertEquals(7, get(v0));
+    }
+
+    // Write more tests!
+
+    /******************************************************************
+     *
+     * Test close10
+     *
+     *****************************************************************/
+
+    @Test
+    public void close10_aClosest() {
+	run("close10", 8, 13);
+	Assert.assertEquals(8, get(v0));
+    }
+
+    // Write more tests!
+
+
+  /******************************************************************
+   *
+   * Test max1020
+   *
+   *****************************************************************/
+
+  @Test
+  public void max1020_bothIn_a_larger() {
+    run("max1020", 15, 11);
+    Assert.assertEquals(15, get(v0));
+  }
+
+  // Write more tests!
+
+    /******************************************************************
+     *
+     * Test dateFashion
+     *
+     *****************************************************************/
+
+    @Test
+    public void dateFashion_youOk_dateStylish() {
+	run("dateFashion", 5, 10);
+	Assert.assertEquals(2, get(v0));
+    }
+
+    // Write more tests!
+
+  /******************************************************************
+   *
+   * Test redTicket
+   *
+   *****************************************************************/
+
+  @Test
+  public void redTicket() {
+    run("redTicket", 5, 10);
+    Assert.assertEquals(2, get(v0));
+  }
+
+
+} // end class
+
+
